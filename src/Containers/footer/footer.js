74011f01a02be340f153e01eb71fe62b3736d972
Aniket762 <aniketindian8@gmail.com>
2021-04-25 03:14:08 +0530

    frontend v1

---
+++ src/Containers/footer/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SocialMediaHorizontal from '../SocialMedia/SocialMediaHorizontal/SocialMediaHorizontal'
+import './footer.css'
+
+function footer() {
+
+    return (
+        <>
+            <footer className="fadeInUp col-md-12">
+            </footer>
+            <h5 className="text-center copyright"> © Copyrights </h5>
+        </>
+    )
+}
+export default footer
